28b4ad515b07fb5fa6f17ef2ab0aa0fdba60a31f
gcarey <gregg.carey@outlook.com>
2022-03-09 11:48:26 -0600

    API layer complete

--- Play.Catalog/src/Play.Catalog.Service/Controllers/ItemsController.cs
+++ Play.Catalog/src/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -24,10 +24,65 @@ namespace Play.Catalog.Service.Controllers
             return items;
         }
         [HttpGet("{id}")]
-        public ItemDto GetById(Guid id)
+        public ActionResult<ItemDto> GetById(Guid id)
         {
             var item = items.Where(item => item.Id == id).SingleOrDefault();
+
+            if (item is null)
+            {
+                return NotFound();
+            }
+
             return item;
+        }
+
+        [HttpPost]
+        public ActionResult<ItemDto> Post(CreateItemDto createItemDto)
+        {
+            var (name, description, price) = createItemDto;
+            var item = new ItemDto(Guid.NewGuid(), name, description, price, DateTimeOffset.UtcNow);
+            items.Add(item);
+
+            return CreatedAtAction(nameof(GetById), new { id = item.Id }, item);
+        }
+        [HttpPut("{id}")]
+        public IActionResult Put(Guid id, UpdateItemDto updateItemDto)
+        {
+            var (name, description, price) = updateItemDto;
+            var existingItem = items.Where(item => item.Id == id).SingleOrDefault();
+
+            if (existingItem is null)
+            {
+                return NotFound();
+            }
+
+            var updatedItem = existingItem with
+            {
+                Name = name,
+                Description = description,
+                Price = price
+            };
+
+            var index = items.FindIndex(existingItem => existingItem.Id == id);
+            items[index] = updatedItem;
+
+            return NoContent();
+
+        }
+        //DELETE /items/id
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            var index = items.FindIndex(item => item.Id == id);
+
+            if (index < 0)
+            {
+                return NotFound();
+            }
+
+            items.RemoveAt(index);
+            return NoContent();
+
         }
 
     }

c30c6df78b4fe1db7562fac018be7de1cfaa6434
gcarey <gregg.carey@outlook.com>
2022-03-10 15:13:27 -0600

    refactor mongo startup to extension methods

--- Play.Catalog/src/Play.Catalog.Service/Startup.cs
+++ Play.Catalog/src/Play.Catalog.Service/Startup.cs
@@ -23,39 +23,12 @@ namespace Play.Catalog.Service
 {
     public class Startup
     {
-        private ServiceSettings serviceSettings;
-
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //MongoDB settings that save db fields as strings
-            BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
-            BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
-
-            serviceSettings = Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
-
-            //Create single instance of MongoDb for application to use
-            services.AddSingleton(ServiceProvider =>
-            {
-                var mongodbSettings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-                var mongoClient = new MongoClient(mongodbSettings.ConnectionString);
-                return mongoClient.GetDatabase(serviceSettings.ServiceName);
-            });
-
-            //Register single instance of concrete repo, which uses single db instance created above.
-            //Now that the collection and Type implementation is passed as a ctor parameter, this Singelton is refactored
-            services.AddSingleton<IRepository<Item>>(ServiceProvider =>
-            {
-                var database = ServiceProvider.GetService<IMongoDatabase>();
-                return new MongoRepository<Item>(database, "items");
-            });
+            //Refactor to Repositories.Extensions
+            services.AddMongo().AddMongoRepository<Item>("items");
 
             services.AddControllers(options =>
             {

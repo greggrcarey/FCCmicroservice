771f07c916cf168283f65ebcf9502244f745e7d0
gcarey <gregg.carey@outlook.com>
2022-03-14 11:41:40 -0500

    added circuit breaker

--- Play.Inventory/src/Play.Inventory.Service/Startup.cs
+++ Play.Inventory/src/Play.Inventory.Service/Startup.cs
@@ -38,7 +38,7 @@ namespace Play.Inventory.Service
             .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().WaitAndRetryAsync(
                 5,
                 retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))
-                + TimeSpan.FromMilliseconds(jitter.Next(0,1000)),
+                + TimeSpan.FromMilliseconds(jitter.Next(0, 1000)),
                 onRetry: (outcome, timespan, retryAttempt) =>
                 {
                     var serviceProvider = services.BuildServiceProvider();
@@ -46,9 +46,24 @@ namespace Play.Inventory.Service
                         .LogWarning($"Delaying for {timespan.TotalSeconds} seconds, then making retry {retryAttempt}");
                 }
             ))
+            .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().CircuitBreakerAsync(
+                3,
+                TimeSpan.FromSeconds(15),
+                onBreak: (outcome, timespan) =>
+                {
+                    var serviceProvider = services.BuildServiceProvider();
+                    serviceProvider.GetService<ILogger<CatalogClient>>()?
+                        .LogWarning($"Opening the circuit for {timespan.TotalSeconds} seconds...");
+                },
+                onReset: () =>
+                {
+                    var serviceProvider = services.BuildServiceProvider();
+                    serviceProvider.GetService<ILogger<CatalogClient>>()?
+                        .LogWarning($"Closing the circuit...");
+                }
+            ))
             .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
-            //Builds on previous code to add an exponentially increasing retry plus jitter
-            //The service provider here is just for demonstration and should not be used this way in production.
+
 
             services.AddControllers();
             services.AddSwaggerGen(c =>

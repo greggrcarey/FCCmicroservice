ced06912ce299173803561baf19f0efe5d53f9fe
gcarey <gregg.carey@outlook.com>
2022-03-17 10:50:32 -0500

    Inventory Service consume Catalog Item messages

---
+++ Play.Inventory/src/Play.Inventory.Service/Consumers/CatalogItemUpdatedConsumer.cs
@@ -0,0 +1,48 @@
+using System.Threading.Tasks;
+using MassTransit;
+using Play.Catalog.Contracts;
+using Play.Common;
+using Play.Inventory.Service.Entities;
+
+namespace Play.Inventory.Service.Consumers
+{
+    public class CatalogItemUpdatedConsumer : IConsumer<CatalogItemCreated>
+    {
+
+        private readonly IRepository<CatalogItem> _repository;
+
+        public CatalogItemUpdatedConsumer(IRepository<CatalogItem> repository)
+        {
+            _repository = repository;
+        }
+        public async Task Consume(ConsumeContext<CatalogItemCreated> context)
+        {
+            var message = context.Message;
+            var item = await _repository.GetAsync(message.ItemId);
+
+            if (item is null)
+            {
+                item = new CatalogItem
+                {
+                    Id = message.ItemId,
+                    Name = message.Name,
+                    Description = message.Description
+                };
+
+                await _repository.CreateAsync(item);
+            }
+            else
+            {
+                item.Name = message.Name;
+                item.Description = message.Description;
+
+                await _repository.UpdateAsync(item);
+            }
+
+
+
+
+
+        }
+    }
+}
--- Play.Inventory/src/Play.Inventory.Service/Startup.cs
+++ Play.Inventory/src/Play.Inventory.Service/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Play.Common.MassTransit;
 using Play.Common.MongoDB;
 using Play.Inventory.Service.Clients;
 using Play.Inventory.Service.Entities;
@@ -27,10 +28,48 @@ namespace Play.Inventory.Service
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMongo().AddMongoRepository<InventoryItem>("inventoryitems");
+            services.AddMongo()
+            .AddMongoRepository<InventoryItem>("inventoryitems")
+            .AddMongoRepository<CatalogItem>("catalogitems")
+            .AddMassTransitWithRabbitMq();
+
 
             Random jitter = new Random();
 
+            AddCatalogClient(services, jitter);
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Play.Inventory.Service", Version = "v1" });
+            });
+            //5:54 hrs
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Play.Inventory.Service v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
+        private static void AddCatalogClient(IServiceCollection services, Random jitter)
+        {
             services.AddHttpClient<CatalogClient>(client =>
             {
                 client.BaseAddress = new System.Uri("https://localhost:5001");
@@ -63,35 +102,6 @@ namespace Play.Inventory.Service
                 }
             ))
             .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
-
-
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Play.Inventory.Service", Version = "v1" });
-            });
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Play.Inventory.Service v1"));
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
         }
     }
 }
